8fd5dcd441a604aa2018f426cc0d65a4671a78c9
Hemamalini-Kamaraj <hemamalini.ahkm@gmail.com>
2023-07-29 16:09:26 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import baseUrl from "./api/api";
+import Sigin from "./components/Sigin";
+import Signup from "./components/Signup";
+import ForgotPassword from "./components/ForgotPassword";
+import ResetPassword from "./components/ResetPassword";
+import AccVerification from "./components/AccVerification";
+import Dashboard from "./components/Dashboard";
+import LogOut from "./components/LogOut";
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState("");
+  const [logInToken, setLogInToken] = useState("");
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const loggedInUserJson = window.sessionStorage.getItem("loggedInUser");
+    if (loggedInUserJson) {
+      const user = JSON.parse(loggedInUserJson);
+      setLoggedInUser(user);
+      setLogInToken(user.token);
+    }
+  }, []);
+
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+
+    const userDetails = {
+      email,
+      password,
+    };
+
+    try {
+      const response = await baseUrl.post("/users/signin", userDetails);
+
+      const user = response.data;
+      window.sessionStorage.setItem("loggedInUser", JSON.stringify(user));
+      setLoggedInUser(user.name);
+      setLogInToken(user.resetToken);
+      setEmail("");
+      setPassword("");
+      navigate("/dashboard");
+    } catch (error) {
+      alert(`Check your email and password ${error} `);
+    }
+  };
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    setLogInToken("");
+    setLoggedInUser("");
+    window.sessionStorage.clear();
+    navigate("/");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Sigin
+            email={email}
+            setEmail={setEmail}
+            password={password}
+            setPassword={setPassword}
+            handleSignIn={handleSignIn}
+          />
+        }
+      />
+      <Route path="/sign-up" element={<Signup />} />
+      <Route path="/users/acc-verification/:id" element={<AccVerification />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="users/reset-password/:id" element={<ResetPassword />} />
+      {loggedInUser ? (
+        <Route
+          path="/dashboard"
+          element={<Dashboard handleLogout={handleLogout} />}
+        />
+      ) : (
+        <Route path="/dashboard" element={<LogOut />} />
+      )}
+    </Routes>
   );
 }
 
